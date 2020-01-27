8443e89d2d9371f95b43c43edf7831f68ca5b762
Sandra Wara <sakr0903@student.miun.se>
2020-01-27 11:26:04 +0100

    Add project files.

---
+++ WebApplication/wwwroot/js/main.js
@@ -0,0 +1,8 @@
+﻿var myDate = new Date();
+var myDay = myDate.getDay();
+
+// Array of days. 
+var weekday = ['Söndag', 'Måndag', 'Tisdag',
+    'Onsdag', 'Torsdag', 'Fredag', 'Lördag'
+];
+document.write("Idag är det " + weekday[myDay]);
